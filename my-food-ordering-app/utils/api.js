3909d9e0a2280f2749b6e143a67c8278c4d3bee8
sameer Dangi <sameerdangi66@gmail.com>
2024-09-07 04:56:39 +0530

    handle proxy error

--- my-food-ordering-app/utils/api.js
+++ my-food-ordering-app/utils/api.js
@@ -1,5 +1,6 @@
-const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const PROXY_URL = "https://thingproxy.freeboard.io/fetch/"; // Alternative CORS proxy
 
 export const MAIN_API = `${PROXY_URL}https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.9690247&lng=72.8205292&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
-export const MENU_LIST_API = (restaurantId) => `${PROXY_URL}https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.9690247&lng=72.8205292&restaurantId=${restaurantId}`;
+export const MENU_LIST_API = (restaurantId) =>
+  `${PROXY_URL}https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.9690247&lng=72.8205292&restaurantId=${restaurantId}`;
